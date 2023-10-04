675a86bbf3a036eedd49414a64d2d7382e8e009d
root <aminoxlhd>
2023-10-03 18:21:50 -0700

    add 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 letter = 0
-for letter1 in range(ord("z"), ord("a") -1 , -1):
+for letter1 in range(ord("z"), ord("a") - 1, -1):
     print("{}".format(chr(letter1 - letter)), end="")
     letter = 32 if letter == 0 else 0
