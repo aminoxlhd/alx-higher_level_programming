470a4ee45a27cb113a8a33a4911039f85176826a
root <aminoxlhd>
2023-10-05 16:22:30 -0700

    add 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,8 @@
 if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 arguments.")
-    else:
-        print("{} arguments.".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{} arguments{}{}".format(
+        count, "s" if count != 1 else "", ":" if count else "."))
+
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
