602e87fdbe0bcb03efc96b4bc1507f57f0ed219e
root <aminoxlhd>
2023-10-05 18:48:20 -0700

    edit 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, div, mul
     import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    operator = {"+": add, "-": sub, "x": mul, "/": div}
     if sys.argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
