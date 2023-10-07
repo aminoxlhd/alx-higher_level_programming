53d63570ce32792b5e477e534a853c1533003ddb
root <aminoxlhd>
2023-10-07 09:41:34 -0700

    edit 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for mtx in matrix:
-        for i in mtx:
-            print("{:d}".format(i), end=" " if i != mtx[-1] else "")
-            print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+        print()
