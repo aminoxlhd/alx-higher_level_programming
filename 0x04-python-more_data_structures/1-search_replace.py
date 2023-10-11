2e90d0f2b28586e49232d5dc902fd913b3e6374f
root <aminoxlhd>
2023-10-11 11:31:39 -0700

    add 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
-            if my_list[i] == search:
-                new_list.append(replace)
-            else:
-                new_list.append(my_list[i])
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
     return new_list
