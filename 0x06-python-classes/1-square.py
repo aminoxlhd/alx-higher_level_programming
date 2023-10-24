b25f4b007ff1a063de701ffbae60c93058757ea0
root <aminoxlhd>
2023-10-24 11:34:01 -0700

    add 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+""" Define a class Square """
+
+
 class Square():
-    def __init__(self, size):
+    """ representing class Square """
+
+
+
+    def ___init___(self, size):
+    """ Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square """
+
+
         self.__size = size
