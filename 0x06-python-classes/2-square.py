35b6d1ab235364841f2d0deedd1c247f3cfa8321
root <aminoxlhd>
2023-10-24 16:56:23 -0700

    edit 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+""" class Square that defines"""
+
 class Square():
+    """ Square class """
+
+
     def __init__(self, size=0):
+        """ Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+            """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
