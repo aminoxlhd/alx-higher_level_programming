878a70204474f993b18ac1b00f501d6920bf51f2
root <aminoxlhd>
2023-10-30 18:18:04 -0700

    edit 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,10 +3,10 @@
 
 
 class Rectangle():
-
+    """" Represent a rectangle
+    number_of_instances (int): The number of Rectangle instances"""
     number_of_instances = 0
 
-    """" Represent a rectangle """
     def __init__(self, width=0, height=0):
         type(self).number_of_instances += 1
         self.width = width
