2971a385b3b326723c33ebe1af1cab7c34ae7ff2
root <aminoxlhd>
2023-10-30 18:20:04 -0700

    edit 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,7 +3,9 @@
 
 
 class Rectangle():
-    """" Represent a rectangle """
+    """" Represent a rectangle 
+    number_of_instances (int): The number of Rectangle instances
+    print_symbol (any): The symbol used for string representation"""
     number_of_instances = 0
     print_symbol = "#"
 
