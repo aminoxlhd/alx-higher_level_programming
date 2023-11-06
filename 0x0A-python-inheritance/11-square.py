577c981cbebf775979f8a9edf8e2548949e875f1
root <aminoxlhd>
2023-11-06 15:29:54 -0800

    add 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ function in Square class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
