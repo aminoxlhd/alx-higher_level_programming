9b3e1d130753b4d9e0b68f9b7004a8555016b774
root <aminoxlhd>
2023-11-06 12:27:54 -0800

    add 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines function """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Check if an object is exactly an instance of a given class """
+    if isinstance(obj, a_class):
+        return True
+    return False
