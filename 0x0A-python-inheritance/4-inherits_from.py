51ff7ebd2bba1d5df781c228be3f60864ff57a0f
root <aminoxlhd>
2023-11-06 12:40:08 -0800

    add 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines function """
+
+
+def inherits_from(obj, a_class):
+    """ Check if an object is exactly an instance of a given class """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
