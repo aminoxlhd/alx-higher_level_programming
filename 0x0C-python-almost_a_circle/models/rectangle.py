c54e86f98cb02d83fb5c485f899d3096151500ad
root <aminoxlhd>
2023-11-13 15:36:56 -0800

    add rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Defines class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle class """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__int__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
