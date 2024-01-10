116f97055f48dfe3621c9e08c957dc1eb5eae9f1
aminoxlhd <aminoxlhd@gmail.com>
2024-01-10 09:00:46 -0800

    edit 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,11 +3,9 @@
 const argv = process.argv.length;
 
 if (argv === 2) {
-	console.log('No argument');
-}
-else if (argv === 3) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+  console.log('No argument');
+} else if (argv === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
