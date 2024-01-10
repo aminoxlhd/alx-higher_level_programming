b2ba4b0bc71de369ee5a2191352f52c1d1b0f01b
aminoxlhd <aminoxlhd@gmail.com>
2024-01-10 09:16:50 -0800

    add 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,8 +3,6 @@ function add (a, b) {
   return a + b;
 }
 
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-const result = add(num1, num2);
-
-console.log(result || 'NaN');
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+console.log(add(a, b));
