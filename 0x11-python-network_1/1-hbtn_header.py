fbb7982d937071cf0b6addbdc9a50b8def0b9866
aminoxlhd <aminoxlhd@gmail.com>
2024-01-26 15:54:02 -0800

    add 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" takes in a URL, sends a request to the URL """
+""" 
+takes in a URL, 
+sends a request to the URL 
+the X-Request-Id variable found in the header of the response.
+"""
 
 import sys
 import urllib.request
