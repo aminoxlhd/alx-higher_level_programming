71f2239ca3f5b6022be756b3130792b33f3d9d21
aminoxlhd <aminoxlhd@gmail.com>
2024-01-26 16:07:15 -0800

    add 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,7 @@ import sys
 import urllib.request
 
 url = sys.argv[1]
+
 request = urllib.request.Request(url)
 with urllib.request.urlopen(request) as response:
     print(dict(response.headers).get("X-Request-Id"))
