5c80766f67536133dded049b19d3191cf4250a31
aminoxlhd <aminoxlhd@gmail.com>
2024-01-26 16:37:32 -0800

    add 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""takes in a URL, 
-sends a request to the URL 
-the X-Request-Id variable found in the header of the response.
+"""takes in a URL
+sends a request to the URL
+the X-Request-Id variable found in the header of the response
 """
 
 import sys
