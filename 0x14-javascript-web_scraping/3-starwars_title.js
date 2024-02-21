d5d36909f5fbf626cf823a917fe038a94e18e976
aminoxlhd <aminoxlhd@gmail.com>
2024-02-20 16:24:27 -0800

    add 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
   console.log(error || JSON.parse(body).title);
 });
